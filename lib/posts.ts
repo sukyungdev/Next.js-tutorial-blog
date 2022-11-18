d4069b12203f17f8068997ac9d725b8aa46f1222
sukyungdev <sukyung.developer@gmail.com>
2022-11-18 16:48:05 +0900

    add : posts.ts

---
+++ lib/posts.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export function getSortedPostsData() {
+  // 포스트 파일
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  const allPostsData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, '');
+
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContent = fs.readFileSync(fullPath, 'utf-8');
+    const matterResult = matter(fileContent);
+
+    return {
+      id,
+      ...(matterResult.data as { date: string; title: string }),
+    };
+  });
+
+  // 포스트 파일 정렬
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
